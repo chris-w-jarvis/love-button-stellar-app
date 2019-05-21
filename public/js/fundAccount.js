42c1e07905fec1b1a64e90ce7db1de5d32f2fbaa
Chris Jarvis <chrisjarvis@Chriss-MacBook-Air.local>
2019-05-21 06:43:31 -0700

    added rate limiter, added fund account logic

--- public/js/fundAccount.js
+++ public/js/fundAccount.js
@@ -16,6 +16,32 @@ $( document ).ready(function () {
                 lBPublicAddr.value = res.loveButtonPublicAddress
                 memo.value = res.memo
                 balance.innerHTML = res.balance
+                var oldBal = parseInt(balance.innerHTML)
+
+                const stopTrigger = setInterval(() => {
+                    // check balance after 3 seconds
+                    console.log('checking')
+                    $.get(
+                        {
+                            url: '/api/checkBalance',
+                            beforeSend: function(xhr){xhr.setRequestHeader('Authorization', token);},
+                            success: function(res) {
+                                console.log(res)
+                                if (parseInt(res.balance) > oldBal) {
+                                    alert("Stellar recieved!")
+                                    balance.innerHTML = res.balance
+                                    clearInterval(stopTrigger)
+                                } else if (parseInt(res.balance) < oldBal) {
+                                    oldBal = parseInt(res.balance)
+                                    balance.innerHTML = res.balance
+                                }
+                            },
+                            error: function() {
+                                console.log("Check balance request failed")
+                            }
+                        }
+                    )
+                }, 3000)
             },
             error: function() {
                 console.log("Fund account request failed")
